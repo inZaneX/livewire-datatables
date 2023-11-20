98c6942a141531a518f8b2b0688a19f9c54df44e
inZaneX <ntrambauer@gmail.com>
2023-11-20 16:34:02 +0100

    Edit for Livewire 3.0

--- src/Livewire/LivewireDatatable.php
+++ src/Livewire/LivewireDatatable.php
@@ -289,8 +289,6 @@ class LivewireDatatable extends Component
         if ($this->persistSearch) {
             session()->put($this->sessionStorageKey() . '_search', $this->search);
         }
-
-        return parent::dehydrate(); // @phpstan-ignore-line
     }
 
     public function columns()
