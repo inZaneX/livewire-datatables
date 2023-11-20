c7679828dabeb9cb1f38a79f4a0551808644129a
inZaneX <ntrambauer@gmail.com>
2023-11-20 16:14:15 +0100

    Edit for Livewire 3.0

--- src/Http/Livewire/LivewireDatatable.php
+++ src/Http/Livewire/LivewireDatatable.php
@@ -1027,7 +1027,7 @@ class LivewireDatatable extends Component
         $this->setPage(1);
         $this->setSessionStoredFilters();
 
-        $this->emitTo('complex-query', 'resetQuery');
+        $this->dispatch('complex-query', 'resetQuery');
     }
 
     public function removeBooleanFilter($column)
@@ -1725,7 +1725,7 @@ class LivewireDatatable extends Component
 
     public function render()
     {
-        $this->emit('refreshDynamic');
+        $this->dispatch('refreshDynamic');
 
         if ($this->persistPerPage) {
             session()->put([$this->sessionStorageKey() . '_perpage' => $this->perPage]);
